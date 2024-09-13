8234db1c1f5a7dcefd2f5821abfd4b59229dca6e
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-13 15:34:30 +0000

    Strucures folders and sets up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/post-list/post-list.component').then(
+        (m) => m.PostListComponent
+      ),
+  },
+  {
+    path: 'posts/:id',
+    loadComponent: () =>
+      import('./pages/post-detail/post-detail.component').then(
+        (m) => m.PostDetailComponent
+      ),
+  },
+];
