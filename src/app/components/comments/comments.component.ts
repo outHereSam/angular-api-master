a1db49fd431f6f560997f25891f10ef9503c21a9
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-15 15:03:23 +0000

    Styles comments section

---
+++ src/app/components/comments/comments.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { Comment } from '../../interfaces/IComment';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../services/api.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-comments',
+  standalone: true,
+  imports: [AsyncPipe],
+  templateUrl: './comments.component.html',
+  styleUrl: './comments.component.sass',
+})
+export class CommentsComponent {
+  @Input() postId: number = 0;
+  comments$: Observable<Comment[]>;
+
+  constructor(private apiService: ApiService) {
+    this.comments$ = this.apiService.getCommentsByPostId(this.postId);
+  }
+
+  ngOnInit() {
+    this.comments$ = this.apiService.getCommentsByPostId(this.postId);
+    this.comments$.subscribe((comments) => console.log(comments));
+  }
+}
--- src/app/pages/post-detail/post-detail.component.ts
+++ src/app/pages/post-detail/post-detail.component.ts
@@ -5,16 +5,17 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ApiService } from '../../services/api.service';
 import { PostCardComponent } from '../../components/post-card/post-card.component';
 import { AsyncPipe } from '@angular/common';
+import { CommentsComponent } from '../../components/comments/comments.component';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [PostCardComponent, RouterLink, AsyncPipe],
+  imports: [PostCardComponent, RouterLink, AsyncPipe, CommentsComponent],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.sass',
 })
 export class PostDetailComponent {
-  post$: Observable<any>;
+  post$: Observable<Post>;
   postId: number = 0;
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) {
