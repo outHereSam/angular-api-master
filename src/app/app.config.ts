65ee38cb76aed4b7b208c32be032384aa6888475
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-09-15 17:22:41 +0000

    Adds logging interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,24 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { postReducer } from './state/posts/posts.reducer';
 import { PostEffects } from './state/posts/posts.effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { loggingInterceptor } from './interceptors/logging.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
     provideStore(),
     provideState({
-        name: 'posts',
-        reducer: postReducer,
+      name: 'posts',
+      reducer: postReducer,
     }),
     provideEffects(PostEffects),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-],
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
